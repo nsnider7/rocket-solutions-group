6060a5f472f5002f3437218ff22afccc5b854223
Tayyab <36415329+MHuzaifa94@users.noreply.github.com>
2022-09-06 21:49:28 +0500

    Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,65 +122,65 @@
         /* ==================================================
             # Magnific popup init
          ===============================================*/
-        $(".popup-link").magnificPopup({
-            type: 'image',
-            // other options
-        });
+        // $(".popup-link").magnificPopup({
+        //     type: 'image',
+        //     // other options
+        // });
 
-        $(".popup-gallery").magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            },
-            // other options
-        });
+        // $(".popup-gallery").magnificPopup({
+        //     type: 'image',
+        //     gallery: {
+        //         enabled: true
+        //     },
+        //     // other options
+        // });
 
-        $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
-            type: "iframe",
-            mainClass: "mfp-fade",
-            removalDelay: 160,
-            preloader: false,
-            fixedContentPos: false
-        });
+        // $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+        //     type: "iframe",
+        //     mainClass: "mfp-fade",
+        //     removalDelay: 160,
+        //     preloader: false,
+        //     fixedContentPos: false
+        // });
 
-        $('.magnific-mix-gallery').each(function() {
-            var $container = $(this);
-            var $imageLinks = $container.find('.item');
+        // $('.magnific-mix-gallery').each(function() {
+        //     var $container = $(this);
+        //     var $imageLinks = $container.find('.item');
 
-            var items = [];
-            $imageLinks.each(function() {
-                var $item = $(this);
-                var type = 'image';
-                if ($item.hasClass('magnific-iframe')) {
-                    type = 'iframe';
-                }
-                var magItem = {
-                    src: $item.attr('href'),
-                    type: type
-                };
-                magItem.title = $item.data('title');
-                items.push(magItem);
-            });
+        //     var items = [];
+        //     $imageLinks.each(function() {
+        //         var $item = $(this);
+        //         var type = 'image';
+        //         if ($item.hasClass('magnific-iframe')) {
+        //             type = 'iframe';
+        //         }
+        //         var magItem = {
+        //             src: $item.attr('href'),
+        //             type: type
+        //         };
+        //         magItem.title = $item.data('title');
+        //         items.push(magItem);
+        //     });
 
-            $imageLinks.magnificPopup({
-                mainClass: 'mfp-fade',
-                items: items,
-                gallery: {
-                    enabled: true,
-                    tPrev: $(this).data('prev-text'),
-                    tNext: $(this).data('next-text')
-                },
-                type: 'image',
-                callbacks: {
-                    beforeOpen: function() {
-                        var index = $imageLinks.index(this.st.el);
-                        if (-1 !== index) {
-                            this.goTo(index);
-                        }
-                    }
-                }
-            });
-        });
+        //     $imageLinks.magnificPopup({
+        //         mainClass: 'mfp-fade',
+        //         items: items,
+        //         gallery: {
+        //             enabled: true,
+        //             tPrev: $(this).data('prev-text'),
+        //             tNext: $(this).data('next-text')
+        //         },
+        //         type: 'image',
+        //         callbacks: {
+        //             beforeOpen: function() {
+        //                 var index = $imageLinks.index(this.st.el);
+        //                 if (-1 !== index) {
+        //                     this.goTo(index);
+        //                 }
+        //             }
+        //         }
+        //     });
+        // });
 		
 
         /* ==================================================
